9b6be06b17cbea9e03d0076c3e64dbec1d029462
Eduardo Gandolfo <eduardo@uizzy.com.br>
2020-11-10 15:24:56 -0300

    vai

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
+// const axios = require('axios');
+
 export const greet = (name: string) => `Hello ${name}`;
+
+interface Log {
+    type?: string
+    details?: string
+    action?: string
+    process?: string
+    date?: Date
+    user_id?: string
+    before?: JSON
+    after?: JSON
+    origen_id?: string
+    username?: string
+    system?: boolean
+}
+
+export const makeLog = (log: Log, config: FactoryLog) => {
+    console.log('makeLog: ', log);
+    console.log(config);
+}
+
+interface FactoryLog {
+    endpoint: string
+}
+
+export const factoryMakeLog = (config: FactoryLog) => {
+    return {
+        makeLog(log: Log) {
+            makeLog(log, config);
+        }
+    }
+}
